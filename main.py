4469bad65006c090c9d5200514be08bf14454136
Filip Sowinski <97602694+sowinskifilip@users.noreply.github.com>
2022-05-13 21:33:19 +0200

    Legs connection draw

--- main.py
+++ main.py
@@ -9,17 +9,6 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 def calculateAngle(landmark1, landmark2, landmark3):
-    '''
-    This function calculates angle between three different landmarks.
-    Args:
-        landmark1: The first landmark containing the x,y and z coordinates.
-        landmark2: The second landmark containing the x,y and z coordinates.
-        landmark3: The third landmark containing the x,y and z coordinates.
-    Returns:
-        angle: The calculated angle between the three landmarks.
-
-    '''
-
     # Get the required landmarks coordinates.
     x1, y1 = landmark1
     x2, y2 = landmark2
@@ -37,9 +26,25 @@ def calculateAngle(landmark1, landmark2, landmark3):
     # Return the calculated angle.
     return angle
 
+# def drawLandmarks(frame, knee_landmarks):
+#
+#     line_color = (255, 255, 255)
+#     line_width = 10
+#
+#     for el in knee_landmarks:
+#         el[0] = int(el[0])
+#         el[1] = int(el[1])
+#
+#     x1, y1 = knee_landmarks[0]
+#     x2, y2 = knee_landmarks[1]
+#     x3, y3 = knee_landmarks[2]
+#
+#     cv2.circle(frame, (x1, y1), 10, line_color, line_width)
+#
+#     return
+
 def CameraCapture():
     cap = cv2.VideoCapture(0)
-    # Initiate holistic model
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
 
         while cap.isOpened():
@@ -51,48 +56,32 @@ def CameraCapture():
             results = pose.process(image)
             # print(results.face_landmarks)
 
-            # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
-
             # Recolor image back to BGR for rendering
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            # # Draw face landmarks
-            # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
-            #
-            # # Right hand
-            # mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-            #
-            # # Left Hand
-            # mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+            right_knee_landmarks = []
+            left_knee_landmarks = []
+            leg_connections = ([(24, 26), (26, 28), (23, 25), (25, 27)])
 
-            # Pose Detections
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-
-            cv2.imshow('Raw Webcam Feed', image)
-
-            right_leg_landmarks = []
 
             # Check if any landmarks are detected.
             if results.pose_landmarks:
-                # Iterate over the detected landmarks.
-                # for landmark in results.pose_landmarks.landmark:
-                #     print(landmark)
 
-                for i in range(24,30,2):
-                    # Display the found landmarks after converting them into their original scale.
-
-                    # print(f'{mp_pose.PoseLandmark(i).name}:')
-                    # print(f'x: {results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].x}')
-                    # print(f'y: {results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].y}')
-
-                    # right_leg_landmarks.append({'x' : results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].x,
-                    #                             'y' : results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].y})
-
-                    right_leg_landmarks.append([results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].x,
+                for (i, j) in zip(range(24,30,2), range(23,29,2)):
+                    right_knee_landmarks.append([results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].x,
                                                results.pose_landmarks.landmark[mp_pose.PoseLandmark(i).value].y])
 
-                angle = calculateAngle(right_leg_landmarks[0], right_leg_landmarks[1], right_leg_landmarks[2])
-                print(angle)
+                    left_knee_landmarks.append([results.pose_landmarks.landmark[mp_pose.PoseLandmark(j).value].x,
+                                               results.pose_landmarks.landmark[mp_pose.PoseLandmark(j).value].y])
+
+                right_angle = calculateAngle(right_knee_landmarks[0], right_knee_landmarks[1], right_knee_landmarks[2])
+                left_angle = calculateAngle(left_knee_landmarks[0], left_knee_landmarks[1], left_knee_landmarks[2])
+                # print('Left knee angle:', left_angle, '; Right knee angle:', right_angle)
+
+                # Pose Detections
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, leg_connections, landmark_drawing_spec=None)
+
+            cv2.imshow('Knee angle measurment', image)
 
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
@@ -106,7 +95,6 @@ def CameraCapture():
 def main():
     CameraCapture()
 
-    # angle = calculateAngle((558, 326, 0), (642, 333, 0), (718, 321, 0))
     return
 
 if __name__ == '__main__':
