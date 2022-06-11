30c1e767e6263a037195335aa49c3836afc41d67
AdamPaech <94913217+AdamPaech@users.noreply.github.com>
2022-06-11 14:52:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from kivy.core.window import Window
 from kivy.clock import Clock
 from datetime import datetime
 from datetime import date
-
+###koment
 
 ###*Defininig the window size*###
 Window.size = (850, 750)
