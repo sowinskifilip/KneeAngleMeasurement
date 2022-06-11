af9169ec792b2fa298682b6720e8ea88897ad0f5
Filip Sowinski <97602694+sowinskifilip@users.noreply.github.com>
2022-06-11 11:23:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.core.window import Window
 from kivy.clock import Clock
 
-###TO JEST MOJ KOMENT####
 
 ###*Defininig the window size*###
 Window.size = (850, 750)
